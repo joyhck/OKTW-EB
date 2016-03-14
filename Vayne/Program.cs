99fd727653742df33b8390a2ee26589e391faa22
berbb <tintinridge@gmail.com>
2016-03-14 17:24:25 -0500

    Don't AA in R feature

--- Vayne/Program.cs
+++ Vayne/Program.cs
@@ -420,13 +420,19 @@ namespace Vayne
             {
                 Orbwalker.ForcedTarget = possible2WTarget;
             }
-            if (myHero.HasBuff("vaynetumblefade") && DontAttackWhileInvisibleAndMeelesNearBool)
+
+            if (myHero.HasBuff("vaynetumblefade"))
             {
-                if (EntityManager.Heroes.Enemies.Any(e => e.ServerPosition.Distance(myHero.ServerPosition) < 350 && e.IsMelee))
+                if (myHero.CountEnemiesInRange(750) >= dontaainr && dontaa)
+                {
+                    args.Process = false;
+                }
+                if (EntityManager.Heroes.Enemies.Any(e => e.ServerPosition.Distance(myHero.ServerPosition) < 350 && e.IsMelee) && DontAttackWhileInvisibleAndMeelesNearBool)
                 {
                     args.Process = false;
                 }
             }
+
             if (myHero.HasBuff("vaynetumblebonus") && args.Target is Obj_AI_Minion && UseQBonusOnEnemiesNotCS)
             {
                 var possibleTarget = TargetSelector.GetTarget(-1f, DamageType.Physical);
@@ -1386,6 +1392,9 @@ namespace Vayne
         public static bool Cnoqenemies { get { return QSettings["Cnoqenemies"].Cast<CheckBox>().CurrentValue; } }
         public static bool Cnoqenemiesold { get { return QSettings["Cnoqenemiesold"].Cast<CheckBox>().CurrentValue; } }
         public static bool Cqspam { get { return QSettings["Cqspam"].Cast<CheckBox>().CurrentValue; } }
+        public static int dontaainr { get { return ComboMenu["dontaainr"].Cast<Slider>().CurrentValue; } }
+        public static bool dontaa { get { return ComboMenu["dontaa"].Cast<KeyBind>().CurrentValue; } }
+
         #endregion
 
         #region Menu
@@ -1410,6 +1419,9 @@ namespace Vayne
             ComboMenu.AddSeparator();
             ComboMenu.AddLabel("1 : Never | 2 : E-Not-Ready | 3 : Always");
             ComboMenu.Add("qantigc", new Slider("Use Q Antigapcloser:", 3, 1, 3)); // UseQAntiGapcloserStringList
+            ComboMenu.AddSeparator();
+            ComboMenu.Add("dontaa", new KeyBind("Don't Auto Attack in R", false, KeyBind.BindTypes.PressToggle, 'T'));
+            ComboMenu.Add("dontaainr", new Slider("Don't Auto Attack in R if >= X enemies", 4, 1, 5));
             ComboMenu.AddSeparator();
 
             QSettings = Menu.AddSubMenu("Q Settings", "qsettings");
