b62e6e2b3a33244b0468d40ce5cae2ac78b00038
berbb <tintinridge@gmail.com>
2016-03-19 00:42:15 -0500

    PortAIO

---
+++ PortAIO/Init.cs
@@ -0,0 +1,31 @@
+﻿#region
+
+using System;
+using EloBuddy;
+using EloBuddy.SDK.Events;
+using EloBuddy.SDK.Menu;
+
+#endregion
+
+namespace BerbsicAIO
+{
+    internal static class Init
+    {
+        private static void Main(string[] args)
+        {
+            Loading.OnLoadingComplete += Initialize;
+        }
+
+        private static void Initialize(EventArgs args)
+        {
+            switch (ObjectManager.Player.ChampionName.ToLower())
+            {
+                case "aatrox": // BrianSharp's Aatrox
+                    BerbsicAIO.Champion.Aatrox.Program.Main();
+                    break;
+                default:
+                    return;
+            }
+        }
+    }
+}
